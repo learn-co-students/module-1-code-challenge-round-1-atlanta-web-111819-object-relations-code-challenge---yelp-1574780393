01464b91e3074220db6e5b051c2825c9f5c5b892
Jessica Michael <pantherjmm@yahoo.comgit config --global user.name Jessica>
2019-11-26 10:58:45 -0500

    crushed it

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,21 @@
 class Customer
-  attr_accessor :given_name, :family_name
+  attr_accessor :given_name, :family_name, :review
+  @@all = []
 
-  def initialize(given_name, family_name)
+  def initialize(given_name, family_name, review)
     @given_name = given_name
     @family_name  = family_name
+    @review = review
+    @@all << self 
+  end
+  def self.all
+    @@all
+  end
+  def customer
+     self.customer do |cust1| customer
   end
 
   def full_name
-    "#{given_name} #{family_name}"
+    "#{given_name} #{family_name}" 
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,22 @@
+require 'pry'
 require_relative '../config/environment.rb'
 
 def reload
   load 'config/environment.rb'
 end
+
+jess_michael=(cust1)
+doctor_dre=(cust2)
+
+chilis=(rest)
+
+rating
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
 binding.pry
+
 0 #leave this here to ensure binding.pry isn't the last line
