d72e892fc52d23ee9b85fed1756ce60366719859
RobertTTaylor9517 <robt15@hotmail.com>
2019-11-26 10:54:22 -0500

    crushed it

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,19 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select{|rv| rv.restaurant == self}.flatten
+  end
+
+  def customers
+    Review.all.select{|rv| rv.restaurant == self}.flatten.map{|rv| rv.customer}
+  end
+
+  def average_star_rating
+    rates = self.reviews.map{|rv| rv.rating}.flatten
+    # binding.pry
+    (rates.sum)/rates.length
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("Robert", "Taylor")
+customer2 = Customer.new("Kara", "Smith")
+customer3 = Customer.new("Eric", "Haber")
+customer4 = Customer.new("Robert", "Clark")
+
+restaurant1 = Restaurant.new("Food Terminal")
+restaurant2 = Restaurant.new("Taco Veloz")
+restaurant3 = Restaurant.new("Top Golf")
+
+Review.new(customer1, restaurant2, 5)
+Review.new(customer2, restaurant1, 2)
+Review.new(customer3, restaurant1, 3)
+Review.new(customer2, restaurant2, 4)
+Review.new(customer3, restaurant3, 5)
+Review.new(customer3, restaurant2, 1)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
